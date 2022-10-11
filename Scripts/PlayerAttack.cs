2faf31bf917b01db1805990b5d95a575b5caff09
Alejandro Sánchez <alejandro.sanchez124@alu.ulpgc.es>
2022-10-11 12:29:18 +0100

    FIxed jump animations

--- Scripts/PlayerAttack.cs
+++ Scripts/PlayerAttack.cs
@@ -32,12 +32,13 @@ public class PlayerAttack : MonoBehaviour
         foreach (Collider2D enemy in hitEnemies)
         {
             enemy.GetComponent<Enemy>().TakeDamage(characterData.Damage);
-            enemy.GetComponent<Enemy>().transform.position -= new Vector3(-knockback, 0, 0); // This will push the enemy after taking the hit
+            enemy.GetComponent<Enemy>().transform.position += new Vector3(knockback, 0, 0); // This will push the enemy after taking the hit
+            // HAY QUE SOLUCIONAR LO DEL KNOCBACK. SI GOLPEO A UN ENEMIGO A MI IZQUIERDA, DEBERÍA EMPUJAR A LA IZQUIERDA, NO A LA DERECHA
         }
     }
 
     /**
-     * This method will draw the gizmos of the attack
+     * This method will draw the gizmos of the attack in the Unity Editor
     */
     private void OnDrawGizmosSelected()
     {
