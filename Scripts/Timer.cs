1d58d5d02d7d8cf3e908f962c58cf653b890da39
Alejandro Sánchez <alejandro.sanchez124@alu.ulpgc.es>
2022-10-17 09:36:36 +0100

    Added timer

---
+++ Scripts/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    private float timeValue = 5; // 2 min.
+    [SerializeField] private TextMeshProUGUI timerText;
+
+
+    private void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        } else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
